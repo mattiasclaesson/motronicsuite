2d1932a2ea1865bbf4bf479f0cb2c889bee6b5ff
mattiasclaesson <mattiaserikanna@gmail.com>
2018-01-04 23:49:42 +0100

    Compile and release fixes

--- release.bat
+++ release.bat
@@ -1,8 +1,15 @@
 set MotronicSuite.version=1.2.4.0
 devenv Motronic.sln /Rebuild Release /project SetupSuite
 
-mkdir C:\users\mattias\Delivery\Motronic\%MotronicSuite.version%
-xcopy SetupSuite\Release\MotronicSuite.msi C:\users\mattias\Delivery\Motronic\%MotronicSuite.version%\
+pushd SetupSuite\Release\
+"C:\md5sum.exe" \MotronicSuite.msi >> \MotronicSuite.md5
+popd
+
+mkdir z:\Motronic\%MotronicSuite.version%
+xcopy SetupSuite\Release\MotronicSuite.msi z:\Motronic\%MotronicSuite.version%\
+
+echo ^<?xml version="1.0" encoding="utf-8"?^>  > z:\Motronic\version.xml
+echo ^<motronic version="%MotronicSuite.version%"/^> >> z:\Motronic\version.xml
 
 echo ----------------------------------------------------
 git changes
